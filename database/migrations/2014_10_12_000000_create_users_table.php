35df7935cbc4a78fa0742a431fb565af6d21bd42
Ahmed Emon <ahmedemon335198@gmail.com>
2022-01-07 03:20:44 +0600

    Project design is setup completed 80%

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->integer('phone');
+            $table->enum('gender', ['Male', 'Female', 'Others']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->boolean('is_active')->default(0);
+            $table->boolean('is_approve')->default(0);
+            $table->boolean('is_blocked')->default(0);
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
